be00fe2e7ec92ded1eb9e4a733130abc3fd92a84
Kirill Gurov <gurov1997@gmail.com>
2023-01-27 20:42:31 +0200

    Update Auth.js

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "https://api.trekit.nomoredomains.sbs";
-// export const BASE_URL = "http://localhost:3001/api";
+export const BASE_URL = "http://localhost:3001";
 
 const handleResponse = (res) => {
   if (res.ok) {
